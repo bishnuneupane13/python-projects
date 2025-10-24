a4f544c2285c07ba23ffc6b94e0b34ecbf5267ff
bishnuneupane13 <196658773+bishnuneupane13@users.noreply.github.com>
2025-10-25 00:15:40 +0545

    Update table_write.py

--- table_write.py
+++ table_write.py
@@ -1,5 +1,3 @@
-import os
-
 n = int(input("Enter a number to create a multiplication table: "))
 
 def create_table():
@@ -8,12 +6,9 @@ def create_table():
         final = n * i
         line = f"{n} x {i} = {final}"
         result.append(line)
+    return "\n".join(result)
 
 table_text = create_table()
 
-# Ensure output directory exists
-os.makedirs('multiply')
-
-# Create the table and save it to file
 with open(f"multiply/multiplication_table_{n}.txt", "w") as file:
     file.write(f"{table_text}")
